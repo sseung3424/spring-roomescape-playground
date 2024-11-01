7668352342fd4e6e9e06b168a1fabb0fd0009020
csh3424 <csh3424@icloud.com>
2024-11-02 08:59:44 +0900

    Feat: 3단계 예약 추가 및 취소 구현

--- src/main/java/roomescape/Controller/ReservationController.java
+++ src/main/java/roomescape/Controller/ReservationController.java
@@ -1,28 +1,45 @@
 package roomescape.Controller;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.Domain.Reservation;
 
 @RestController
 public class ReservationController {
   private List<Reservation> reservations = new ArrayList<>();
+  private AtomicLong index = new AtomicLong(1);
 
-  public ReservationController() {
-    Reservation reservation1 = new Reservation(1, "브라운", "2023-01-01", "10:00");
-    Reservation reservation2 = new Reservation(2, "브라운", "2023-01-02", "11:00");
-    Reservation reservation3 = new Reservation(3, "브라운", "2023-01-03", "12:00");
-
-    reservations.add(reservation1);
-    reservations.add(reservation2);
-    reservations.add(reservation3);
+  @PostMapping("/reservations")
+  public ResponseEntity<Reservation> create(@RequestBody Reservation reservation) {
+    Reservation newReservation = Reservation.toEntity(reservation, index.getAndIncrement());
+    reservations.add(newReservation);
+    return ResponseEntity.created(URI.create("/reservations/" + newReservation.getId()))
+        .body(newReservation);
   }
 
   @GetMapping("/reservations")
   public ResponseEntity<List<Reservation>> read() {
     return ResponseEntity.ok().body(reservations);
+  }
+
+  @DeleteMapping("/reservations/{id}")
+  public ResponseEntity<Void> delete(@PathVariable long id) {
+    Reservation reservation = reservations.stream()
+        .filter(it -> Objects.equals(it.getId(), id))
+        .findFirst()
+        .orElseThrow(RuntimeException::new);
+
+    reservations.remove(reservation);
+    return ResponseEntity.noContent().build();
   }
 }
--- src/main/java/roomescape/Domain/Reservation.java
+++ src/main/java/roomescape/Domain/Reservation.java
@@ -1,21 +1,21 @@
 package roomescape.Domain;
 
 public class Reservation {
-  private Integer id;
+  private Long id;
   private String name;
   private String date;
   private String time;
 
   public Reservation() {}
 
-  public Reservation(Integer id, String name, String date, String time) {
+  public Reservation(Long id, String name, String date, String time) {
     this.id = id;
     this.name = name;
     this.date = date;
     this.time = time;
   }
 
-  public Integer getId() {
+  public Long getId() {
     return id;
   }
 
@@ -29,5 +29,11 @@ public class Reservation {
 
   public String getTime() {
     return time;
+  }
+
+  public static Reservation toEntity (Reservation reservation, Long id) {
+    return new Reservation(
+        id, reservation.getName(), reservation.getDate(), reservation.getTime()
+    );
   }
 }
